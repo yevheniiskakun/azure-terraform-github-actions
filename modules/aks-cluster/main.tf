a558f3de59379492926ef4c6604d4077ac50653d
yevheniiskakun <yevhenii.skakun@gmail.com>
2025-05-27 21:35:57 +0200

    Added AKS module

---
+++ modules/aks-cluster/main.tf
@@ -0,0 +1,52 @@
+resource "azurerm_kubernetes_cluster" "aks" {
+  name = var.aks_name
+  location = var.location
+  resource_group_name = var.rg_name
+  dns_prefix = var.aks_name
+  role_based_access_control_enabled = true
+  tags = var.tags
+
+  default_node_pool {
+    name = "default"
+    node_count = var.node_count
+    vm_size = var.vm_size
+    type = "VirtualMachineScaleSets"
+    
+    min_count = var.min_count
+    max_count = var.max_count
+  }
+
+  azure_policy_enabled = var.enable_azure_policy
+  oidc_issuer_enabled = true
+  workload_identity_enabled = true
+
+  workload_autoscaler_profile {
+    keda_enabled = true
+  }
+
+  identity {
+    type = "UserAssigned"
+    identity_ids = [ var.identity_ids ]
+  }
+
+  network_profile {
+    network_plugin = "kubenet"
+    load_balancer_sku = "standard"
+  }
+
+  monitor_metrics {
+    annotations_allowed = null
+    labels_allowed = null
+  }
+
+  linux_profile {
+    admin_username = var.username
+    ssh_key {
+      key_data = var.ssh_public_key
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [default_node_pool[0].node_count]
+  }
+}
